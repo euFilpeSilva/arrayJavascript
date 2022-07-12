c51cdfd950c5c31902a040a9220178d5dc0da1a3
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-07-12 07:35:40 -0300

    adicionando mais alguns metodos

--- arrayMetodos.js
+++ arrayMetodos.js
@@ -1,9 +1,36 @@
-const elementos =['elemento1', 'elemento2', 'elemento3', 'elemento4']
+const elementos =['elemento0', 'elemento1', 'elemento2', 'elemento3']
 
-elementos.pop() //remove o ultimo item do array
+elementos.pop() //remove o ultimo item do array.
+
+console.log(elementos);
+
+elementos.push('elemento_adicional') //adiciona um elemento a ultima posição do meu array.
 
 console.log(elementos);
 
-elementos.push('elemento_adicional') //adiciona um elemento a ultima posição do meu array
+elementos.shift() //Remove primeiro elemento da lista.
 
 console.log(elementos);
+
+elementos.unshift('elemento_especial') //Adiciona elemento a primeira posição do array.
+
+console.log(elementos);
+
+//Splice pode tanto adicional como remover elementos.
+
+//Adicionar
+elementos.splice(2, 0, 'esp01', 'esp02' )
+
+//Remover
+elementos.splice(3, 1)
+
+console.log(elementos);
+
+const novoArray = elementos.slice(2) //Gera um novo array a partir do indice passado.
+
+console.log(novoArray);
+
+const indicesRestritos = elementos.slice(1, 4) //Nesse caso vamos imprimir somente do indice 1 até o 3.
+
+console.log(indicesRestritos);
+
