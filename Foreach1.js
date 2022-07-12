4b284bb9b03126536afe4420f74888b9680707cf
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-07-12 07:53:35 -0300

    subindo o forEach

---
+++ Foreach1.js
@@ -0,0 +1,9 @@
+const aprovados = ['Juliana', 'Alto', 'João', 'Marcelo']
+
+
+//O forEach vai percorrer o arrai e pra cada iteração a função vai trazer um nome com o indice incrementado.
+aprovados.forEach(function(nome, indice) {
+    console.log(`${ indice +1 } ${nome}`)
+})
+
+aprovados.forEach(nome => console.log(nome)) //Forma reduzida onde quero só os nomes, sem os indices
