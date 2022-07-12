31a30bd9be6295cf38ae5fe2a63d8dd2b7ba7a0d
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-07-12 07:20:01 -0300

    sobe funcoes pop e push

---
+++ arrayMetodos.js
@@ -0,0 +1,9 @@
+const elementos =['elemento1', 'elemento2', 'elemento3', 'elemento4']
+
+elementos.pop() //remove o ultimo item do array
+
+console.log(elementos);
+
+elementos.push('elemento_adicional') //adiciona um elemento a ultima posição do meu array
+
+console.log(elementos);
