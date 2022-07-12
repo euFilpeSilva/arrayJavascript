217cab14adad448c5f66b3f8cfc3b590e3f9d603
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-07-12 08:18:29 -0300

    atualizando map

--- Map01.js
+++ Map01.js
@@ -4,3 +4,15 @@ let result = numeros.map( e => {
     return e *2
 })
 console.log(result);
+
+//O map gera um novo array, não altera o atual.
+
+//outros examples:
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const convertDinheiro = e => ` R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
+
+result = numeros.map(soma10).map(triplo).map(convertDinheiro)
+
+console.log(result);
