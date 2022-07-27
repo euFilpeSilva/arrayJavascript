492535a81bc873b1f64febf3130b8cdb407a6741
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-07-27 13:32:28 -0300

    atualizando

--- node/funcoes/basico_03.js
+++ node/funcoes/basico_03.js
@@ -6,3 +6,23 @@ felizNatal()
 const saudacao = nome => `Fala ${nome}!!!`
 
 console.log(saudacao("Filipe"));
+
+const somar = numeros => {
+    let total = 0
+    for(let n of numeros) {
+        total += n
+    }
+    return total
+}
+ 
+console.log(somar([1,2,3,4,5,6,7,8,9,10]));
+
+
+// brincando com arrow function
+const potencia = base => {
+    return (exp) => {
+        return Math.pow(base, exp)
+    }
+}
+
+console.log(potencia(2)(8));
