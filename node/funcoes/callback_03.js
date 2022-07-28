31ce8b9d37f35f3c5438b6015cb6243553365816
euFilpeSilva <sousadasilvafilipe@gmail.com>
2022-07-28 20:33:34 -0300

    exemplos de map

---
+++ node/funcoes/callback_03.js
@@ -0,0 +1,34 @@
+const nums = [1, 2, 3, 4, 5]
+const dobro = n => n * 2  //parametro representando cada numero do array
+console.log(nums.map(dobro));
+
+const nums2 = [1, 2, 3, 4, 5]
+const dobro2 = (n, i) => n * 2 + i //segundo parametro representando os indices do array
+console.log(nums2.map(dobro2));
+
+const nums3 = [1, 2, 3, 4, 5]
+const dobro3 = (n, i, a) => n * 2 + i + a.length //Terceiro parametro representando o tamanho do array
+console.log(nums3.map(dobro3));
+
+// Vamos retornas a primeira letra de cada elemento do array nomes
+const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa']
+const primeiraLetra = texto => texto[0]
+const letras = nomes.map(primeiraLetra)
+console.log(nomes, letras);
+
+
+const carrinho = [
+    {nome: 'Caneta', qtde: 10, preco: 649.50 },
+    {nome: 'Impressora', qtde: 9, preco: 789.50 },
+    {nome: 'Caderno', qtde: 3, preco: 69.80 },
+    {nome: 'Lapis', qtde: 4, preco: 4.10 },
+    {nome: 'Tesoura', qtde: 1, preco: 64.30 },
+]
+
+const pegaNome = item => item.nome //retorna o nome de cada item do array
+ console.log(carrinho.map(pegaNome));
+
+ const totalItens = item => item.qtde * item.preco //retorna as quantidades multiplocadas pelos preços
+ const totais = carrinho.map(totalItens)
+
+ console.log(totais);
